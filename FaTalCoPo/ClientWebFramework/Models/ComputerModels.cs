877d66f19b829a8f2184a86eaa9f90ce88db1cfe
szdodo <szeghi.@gmail.com>
2017-09-07 16:00:08 +0200

    Models class for Computer done

---
+++ FaTalCoPo/ClientWebFramework/Models/ComputerModels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClientWebFramework.Models
+{
+    public class ComputerModels
+    {
+        public int IPAddress { get; set; }
+        public int UPTime { get; set; }
+        public string ComputerName { get; set; }
+        public string OSName { get; set; }
+        public string OSVersion { get; set; }
+        public int TotalMemory { get; set; }
+        public int CPU { get; set; }
+        public DateTime InstallDate { get; set; }
+        public string InputLocale { get; set; }
+        public string SystemLocale { get; set; }
+    }
+}
