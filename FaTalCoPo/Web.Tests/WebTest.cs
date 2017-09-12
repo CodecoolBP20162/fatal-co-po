5644390835f7081b15313364bc9fdaf23c68217d
Szilniczky <szilniczky.david@gmail.com>
2017-09-12 16:47:39 +0200

    make test project for the solution and add 1 test

---
+++ FaTalCoPo/Web.Tests/WebTest.cs
@@ -0,0 +1,19 @@
+﻿using ClientWebFramework.Controllers;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace Web.Tests
+{
+    [TestFixture]
+    public class WebTest
+    {
+        [Test]
+        public void LoadComputersIsEmpty()
+        {
+            ComputerController computer = new ComputerController();
+            string result = null;
+            Assert.AreEqual(result, Is.Empty);
+        }
+    }
+}
