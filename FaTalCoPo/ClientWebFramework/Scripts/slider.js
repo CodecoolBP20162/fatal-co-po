8e88aa705bc001598bd62823251b5e224f009770
davidkulchar <davidkulchar@gmail.com>
2017-09-21 14:24:01 +0200

    minimal ui is ready

--- FaTalCoPo/ClientWebFramework/Scripts/slider.js
+++ FaTalCoPo/ClientWebFramework/Scripts/slider.js
@@ -1,1 +1,9 @@
-﻿
+﻿$(document).ready(function () {
+    $('.slider .slider-item').first().addClass('slider-active-item');
+
+
+    $('.slider-item').bind("click", function () {
+        $('.slider-active-item').removeClass('slider-active-item');
+        $(this).addClass('slider-active-item');
+    });
+});
