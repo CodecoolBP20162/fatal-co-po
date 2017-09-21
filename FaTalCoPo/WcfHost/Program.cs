1d6809414c62a3e7826ad2245a7b68f42b63752b
szdodo <szeghi.@gmail.com>
2017-09-21 16:46:32 +0200

    simple logging for wcf host done

--- FaTalCoPo/WcfHost/Program.cs
+++ FaTalCoPo/WcfHost/Program.cs
@@ -11,6 +11,8 @@ namespace WcfHost
 
     class Program
     {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         public static void Main(string[] args)
         {
            WcfTestHost_Open();
@@ -20,25 +22,33 @@ namespace WcfHost
         public static void WcfTestHost_Open()
         {
             string hostname = Dns.GetHostEntry(Dns.GetHostName()).AddressList.First(f => f.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork).ToString();
+            log.Debug("Hostname: " + hostname);
             var baseAddress = new UriBuilder("http", hostname, 2000, "WcfPing");
-            var h = new ServiceHost(typeof(WcfPing), baseAddress.Uri);
+            log.Debug("baseAddress: " + baseAddress);
+            var serviceHost = new ServiceHost(typeof(WcfPing), baseAddress.Uri);
+            log.Debug("New ServiceHost created: " + serviceHost.ToString());
 
             // enable processing of discovery messages.  use UdpDiscoveryEndpoint to enable listening. use EndpointDiscoveryBehavior for fine control.
-            h.Description.Behaviors.Add(new ServiceDiscoveryBehavior());
-            h.AddServiceEndpoint(new UdpDiscoveryEndpoint());
+            serviceHost.Description.Behaviors.Add(new ServiceDiscoveryBehavior());
+            log.Debug("Fine control added to " + serviceHost.ToString());
+            serviceHost.AddServiceEndpoint(new UdpDiscoveryEndpoint());
+            log.Debug("Listening enabled on " + serviceHost.ToString());
 
             // enable wsdl, so you can use the service from WcfStorm, or other tools.
             var smb = new ServiceMetadataBehavior();
             smb.HttpGetEnabled = true;
             smb.MetadataExporter.PolicyVersion = PolicyVersion.Policy15;
-            h.Description.Behaviors.Add(smb);
+            serviceHost.Description.Behaviors.Add(smb);
+            log.Debug("ServiceeMetaDataBehaviour added to " + serviceHost.ToString());
 
             // create endpoint
             var binding = new BasicHttpBinding(BasicHttpSecurityMode.None);
             binding.MaxReceivedMessageSize = 4294967295;
             binding.TransferMode = TransferMode.Streamed;
-            h.AddServiceEndpoint(typeof(IWcfPing), binding, "");
-            h.Open();
+            serviceHost.AddServiceEndpoint(typeof(IWcfPing), binding, "");
+            log.Debug("Endpoint added to " + serviceHost.ToString());
+            serviceHost.Open();
+            log.Debug(serviceHost.ToString() + " is open");
             Console.WriteLine("host open at " + baseAddress);
         }
     }
