e95e3c5cb448e06262be825e9acd39563c3339b8
szdodo <szeghi.@gmail.com>
2017-09-21 14:37:54 +0200

    merge bugs resolved

--- FaTalCoPo/ClientWebFramework/App_Start/BundleConfig.cs
+++ FaTalCoPo/ClientWebFramework/App_Start/BundleConfig.cs
@@ -22,7 +22,7 @@ namespace ClientWebFramework
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js",
-                      "~/Scripts/slider.js"));
+                      "~/Scripts/slider.js",
                       "~/Scripts/load.js"));
           
             bundles.Add(new StyleBundle("~/Content/css").Include(
